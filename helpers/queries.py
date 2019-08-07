92f198a57ecb0b90fa44e266fd7f0af733a41c66
MacMacky <mac21macky@gmail.com>
2019-08-07 10:33:49 +0800

    Add all queries

--- helpers/queries.py
+++ helpers/queries.py
@@ -4,9 +4,19 @@ queries = {
           contactNumber,storeDescription,zipcode,storeDetails,
           storePolicies,paymentMethod,DATE(syscreated) syscreated,sysmodified from `mlshop`.`merchantlist`;""",
   1 : "UPDATE `mlshop`.`merchantlist` SET status = 3 WHERE merchantID = ? AND email = ?;",
-  2 : "UPDATE `mlshop`.`merchantlist`  SET  sellerName = ?,shopName= ?, email = ?, storeAddress = ?, city = ?, country  = ?, contactNumber = ? ,storeDescription = ? WHERE merchantID = ?",
+  2 : "UPDATE `mlshop`.`merchantlist`  SET  sellerName = %s,shopName= %s, email = %s, storeAddress = %s, city = %s, country  = %s, contactNumber = %s ,storeDescription = %s WHERE merchantID = %s",
   3 : """SELECT merchantID,sellerName,shopName,email,password,status,storeAddress,city,country,contactNumber,storeDescription,zipcode,storeDetails,storePolicies,paymentMethod,syscreated,sysmodified
-       from `mlshop`.`merchantlist` WHERE merchantID = ? ;"""
+       from `mlshop`.`merchantlist` WHERE merchantID = %s ;""",
+  4 : "UPDATE `mlshop`.`merchantlist` SET status = 2 WHERE merchantID = %s AND email = %s;",
+  5 : "SELECT * FROM `mlshop`.`Admin` WHERE email = %s AND password = %s AND `status`=1;",
+  6 : "SELECT * FROM `mlshop`.`merchantlist` WHERE sellerName LIKE %s;",
+  7 : "UPDATE `mlshop`.`merchantlist` SET `status` = 1 WHERE merchantID = %s AND email = %s;",
+  8 : "UPDATE `mlshop`.`merchantlist` SET  `password` = %s WHERE merchantID = %s AND email = %s;",
+  9 : "INSERT INTO ??(session_key,isExpired,expiration,created_at,updated_at) VALUES(?,0,(SELECT DATE_ADD(NOW(),INTERVAL 1 HOUR)),NOW(),NOW());",
+  10 : "UPDATE ?? SET isExpired = 1 WHERE session_key = %s",
+  11 : "SELECT NOW() as `date`",
+  12 : "SELECT * from `mlshop`.`merchantlist`;"
+
 }
 
 
