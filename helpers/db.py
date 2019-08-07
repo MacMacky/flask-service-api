db258afe14b0e1925e0827b7d3785ab77b2cc167
MacMacky <mac21macky@gmail.com>
2019-08-07 09:47:43 +0800

    Modify 'pool_size' to have value of '30'

--- helpers/db.py
+++ helpers/db.py
@@ -1,8 +1,8 @@
-import mysql.connector as connection
+import mysql.connector.pooling as pooling
 from config import DBHOST, DBPASS, DBPORT, DBUSER
 
 
-MySQLError = connection.Error
-dbconfig = {"username": DBUSER, "host": DBHOST, "passwd": DBPASS, "port": DBPORT}
-con = connection.MySQLConnection(**dbconfig)
-
+MySQLError = pooling.errors.PoolError
+dbconfig = {"username": DBUSER, "host": DBHOST, "passwd": DBPASS, "port": DBPORT,"pool_size":30}
+#con = connection.MySQLConnection(**dbconfig)
+pool = pooling.MySQLConnectionPool(**dbconfig)
