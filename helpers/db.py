d6416927cf228ce97fcbd067ec926b0863e0c510
MacMacky <mac21macky@gmail.com>
2019-08-07 10:31:23 +0800

    Set 'pool_reset_session' to False and 'dbconfig' dict

--- helpers/db.py
+++ helpers/db.py
@@ -3,6 +3,6 @@ from config import DBHOST, DBPASS, DBPORT, DBUSER
 
 
 MySQLError = pooling.errors.PoolError
-dbconfig = {"username": DBUSER, "host": DBHOST, "passwd": DBPASS, "port": DBPORT,"pool_size":30}
+dbconfig = {"username": DBUSER, "host": DBHOST, "passwd": DBPASS, "port": DBPORT,"pool_size":30,"pool_reset_session":False}
 #con = connection.MySQLConnection(**dbconfig)
 pool = pooling.MySQLConnectionPool(**dbconfig)
