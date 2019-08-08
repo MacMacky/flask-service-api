22d4088d4038a66f5ee615b820db34a80711ec8e
MacMacky <mac21macky@gmail.com>
2019-08-08 13:48:20 +0800

    Update placeholder values

--- helpers/queries.py
+++ helpers/queries.py
@@ -3,7 +3,7 @@ queries = {
           email,password,status,storeAddress,city,country,
           contactNumber,storeDescription,zipcode,storeDetails,
           storePolicies,paymentMethod,DATE(syscreated) syscreated,sysmodified from `mlshop`.`merchantlist`;""",
-  1 : "UPDATE `mlshop`.`merchantlist` SET status = 3 WHERE merchantID = ? AND email = ?;",
+  1 : "UPDATE `mlshop`.`merchantlist` SET status = 3 WHERE merchantID = %s AND email = %s;",
   2 : "UPDATE `mlshop`.`merchantlist`  SET  sellerName = %s,shopName= %s, email = %s, storeAddress = %s, city = %s, country  = %s, contactNumber = %s ,storeDescription = %s WHERE merchantID = %s",
   3 : """SELECT merchantID,sellerName,shopName,email,password,status,storeAddress,city,country,contactNumber,storeDescription,zipcode,storeDetails,storePolicies,paymentMethod,syscreated,sysmodified
        from `mlshop`.`merchantlist` WHERE merchantID = %s ;""",
@@ -12,8 +12,8 @@ queries = {
   6 : "SELECT * FROM `mlshop`.`merchantlist` WHERE sellerName LIKE %s;",
   7 : "UPDATE `mlshop`.`merchantlist` SET `status` = 1 WHERE merchantID = %s AND email = %s;",
   8 : "UPDATE `mlshop`.`merchantlist` SET  `password` = %s WHERE merchantID = %s AND email = %s;",
-  9 : "INSERT INTO ??(session_key,isExpired,expiration,created_at,updated_at) VALUES(?,0,(SELECT DATE_ADD(NOW(),INTERVAL 1 HOUR)),NOW(),NOW());",
-  10 : "UPDATE ?? SET isExpired = 1 WHERE session_key = %s",
+  9 : "INSERT INTO %s(session_key,isExpired,expiration,created_at,updated_at) VALUES(%s,0,(SELECT DATE_ADD(NOW(),INTERVAL 1 HOUR)),NOW(),NOW());",
+  10 : "UPDATE %s SET isExpired = 1 WHERE session_key = %s",
   11 : "SELECT NOW() as `date`",
   12 : "SELECT * from `mlshop`.`merchantlist`;"
 
