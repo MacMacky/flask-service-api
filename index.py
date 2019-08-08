8b5289c928f9ffd6852ad85a2a28b4993c31f7d5
MacMacky <mac21macky@gmail.com>
2019-08-08 13:51:47 +0800

    Add 'disableSeller' route and functionality

--- index.py
+++ index.py
@@ -1,62 +1,117 @@
-from flask import Flask,jsonify,render_template,render_template_string
+import smtplib
 import requests
-import mysql.connector as connection
-from settings import DBHOST,DBPASS,DBPORT,DBUSER
-from helpers.queries import Query
-from typing import Tuple
+from typing import Any
+from flask_caching import Cache
+from helpers.logger import logging
+from helpers import queries, db, main,res_msg
+from flask import Flask, jsonify, render_template, render_template_string, request
 
+cache = Cache(config={"CACHE_TYPE":"simple"})
 app = Flask(__name__)
+cache.init_app(app)
 
-def slice(listo=[],start =0, stop=1):
-   result = []
-   lengtho = len(listo)
-   for i in range(lengtho):
-        if start == stop:
-            break
+MAIN_URL:str = '/mlshopadmin/api/v1/'
+
+@app.route("/")
+@app.route(f"{MAIN_URL}checkConnection")
+@cache.cached(timeout=100)
+def check_connection():
+  try:   
+    con = db.pool.get_connection()
+    if con.is_connected():
+      return main.resJson(ResponseCode=200,ResponseMessage="Success in connecting to Database.")
+    else :
+      return main.resJson(ResponseCode=200,ResponseMessage="Failed in connecting to Database.")
+  except db.MySQLError  as e:
+      return main.resJson(500,e.msg)
+  finally:
+      con.close()
+
+
+@app.route(f"{MAIN_URL}disableSeller",methods=['POST'])
+def disable_seller():
+     con = db.pool.get_connection();
+     cursor = con.cursor();
+     try:
+        body = dict(request.form)
+        email  = body.get('email'); id = body.get('merchantID')
+        msg = res_msg.index(18); code = 200
+        if main.is_none(email) or main.is_none(id):
+           msg = res_msg.index(5); code =463
         else:
-           result.append(listo[i])
-           start += 1
-   return result
-
-@app.route('/')
-@app.route('/home')
-def index():
-   r = requests.get('https://jsonplaceholder.typicode.com/todos')
-   result = slice(r.json(),1,5)
-   print(result)
-   return render_template('main.html',todos=result,title="Flask App")
-   #return render_template_string(template)
-
-@app.route('/herro-world')
-def sample():
-   return "Herro Worl2d!"
+           cursor.execute(queries.Query(1),[id,email])
+           con.commit()
+     except BaseException as e:
+         msg = e.msg; code = 500
+         con.rollback()
+     finally:
+        con.close();cursor.close()
+        return main.resJson(ResponseCode=code,ResponseMessage=msg);
 
 
-@app.route('/route-with-param/<name>')
-def route_with_param(name:str):
-    return "Hello," + name 
+@app.route(f"{MAIN_URL}changePassword")
+def change_password():
+    con = db.pool.get_connection()
+    cursor  = con.cursor()
+    code:int = 200 ;msg:str = res_msg.index(200); result = []
+    try:
+      qry = dict(request.args);
+      email = qry.get('email');new_pass = qry.get('newPassword');id = qry.get('merchantID')
+      if main.is_none(email) or main.is_none(new_pass) or main.is_none(id):
+           msg = res_msg.index(5); code = 463
+      else:
+           msg = res_msg.index(200); code =200
+    except BaseException as e:
+       code = 500 ; msg =e.msg
+    finally:
+       con.close();cursor.close();
+       return main.resJson(ResponseCode=code,ResponseMessage=msg)
 
-@app.route('/send-json')
+
+@app.route("/send-json")
 def send_json():
-    r = requests.get('https://jsonplaceholder.typicode.com/todos')
+    r = requests.get("https://jsonplaceholder.typicode.com/todos")
     return jsonify(r.json())
 
-@app.route('/get_db_data')
-def get_db_data():
-  con = connection.connect(host=DBHOST,port=DBPORT,user=DBUSER,password=DBPASS)
-  result = []
-  try:  
+@app.route(f"{MAIN_URL}searchSeller")
+def search_by_seller():
+    con = db.pool.get_connection()
     cursor = con.cursor()
-    cursor.execute(Query(0))
-    result = cursor.fetchall()
-  except (connection.Error,ArithmeticError) as e:
-    print(e.msg)
-    pass
-  finally:
-     msg = "Is Connected to DB." if con.is_connected() else "Not Connected to DB"
-     con.close()
-     return jsonify(result)
+    try:
+        qry_str = dict(request.args); msg:str = res_msg.index(200)
+        code = 200; result = []
+        if main.is_none(qry_str.get('sellerName')):
+           msg = res_msg.index(5); code = 463
+        else:
+           cursor.execute(queries.Query(6),[qry_str.get('sellerName')])
+           result = cursor.fetchall()
+           result = main.result_dict(cursor.column_names,result)
+           msg = res_msg.index(200); code = 200
+    except BaseException as e:
+        msg = e.msg; code = 500
+    finally:
+        con.close();cursor.close()
+        return main.resJson(ResponseCode=code,ResponseMessage=msg,result=result)
 
-if __name__ == '__main__':
-    app.run(debug=True);
+
+
+@app.route(f"{MAIN_URL}displaySellers")
+def display_sellers():
+    result = []
+    con = db.pool.get_connection()
+    code = 200; msg  = res_msg.index(200)
+    cursor : Any
+    try:
+        cursor = con.cursor()
+        cursor.execute(queries.Query(0))
+        result = cursor.fetchall()
+        result = main.result_dict(cursor.column_names,result)
+    except db.MySQLError as e:
+       code = 500; msg = e.msg
+    finally:
+        con.close(); cursor.close()
+        return main.resJson(ResponseCode=code,ResponseMessage=msg,Seller_List=result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
