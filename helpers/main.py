c1a8fd77056609208181f9efb78da0c1a6cbb396
MacMacky <mac21macky@gmail.com>
2019-08-08 14:00:19 +0800

    Add new helper func 'generate_token_db_name' and add types to other funcs

--- helpers/main.py
+++ helpers/main.py
@@ -1,5 +1,6 @@
 from typing import List,Tuple,Type
 from flask import jsonify
+from datetime import date
 
 def slice(listo: List = [], start: int = 0, stop: int = 1) -> []:
     result: List = []
@@ -31,3 +32,14 @@ def result_dict(column_names:Tuple[str], datas:List[str]) -> List[dict]:
    
 def is_none(val:Type):
     return val == None
+
+
+def generate_token_db_name() -> str:
+    now = date.today()
+    year = now.year
+    day = now.day
+    month = now.month
+    len_m = len(str(month)); len_d = len(str(day))
+    day = f"0{day}" if len_d == 1 else str(day)
+    month = f"0{month}" if len_m == 1 else str(month)
+    return f"shopsession{year}{month}{day}.sessiondetails"
